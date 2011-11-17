b0b2d09eaa63c08a53086fda3778e0666101da5d
Chris Lumens <chris@bangmoney.org>
2011-11-16 23:37:48 -0500

    Add a module to sign exported ADIF files, prior to uploading.

---
+++ Sign.hs
@@ -0,0 +1,23 @@
+module Sign(sign) where
+
+import System.Cmd(system)
+import System.Directory(doesFileExist)
+import System.Exit(ExitCode(..))
+
+-- Given a file path and a QTH, sign the file.  Note that you must have set
+-- everything up with tqsl first.
+--
+-- NOTE:  tqsl is annoying and will start up graphically, especially if there
+-- are any errors.  It's recommended that the QTH be verified by running this
+-- command.  We can at least verify the file path here, though.
+sign :: FilePath -> String -> IO ()
+sign file qth = do
+    exists <- doesFileExist file
+
+    if exists then do
+        rc <- system $ "tqsl -d -l " ++ qth ++ " " ++ file ++ " -x"
+        case rc of
+            ExitSuccess     -> return ()
+            ExitFailure _   -> error "Signing failed."
+    else
+        error "File does not exist."
