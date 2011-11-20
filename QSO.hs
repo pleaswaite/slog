188b8a6035746ffe45c1622ea5be7cd2f03a7b6e
Chris Lumens <chris@bangmoney.org>
2011-11-19 22:09:15 -0500

    Add a function to export a QSO type to an ADIF type.

--- QSO.hs
+++ QSO.hs
@@ -1,5 +1,8 @@
 module QSO where
 
+import Data.Maybe(catMaybes)
+import Utils(freqToBand)
+
 -- Some of these types are pretty useful everywhere.  Perhaps they should move
 -- up into a non-ADIF specific module.
 import qualified Formats.ADIF.Types as ADIF
@@ -39,3 +42,30 @@ data Confirmation = Confirmation {
     qLOTW_Rcvd    :: ADIF.ReceivedStatus,
     qLOTW_Sent    :: ADIF.SentStatus }
  deriving (Eq, Show, Read)
+
+qsoToADIF :: QSO -> [ADIF.Field]
+qsoToADIF qso =
+    -- First, let's get the required fields we know will always exist.
+    [ADIF.QSO_Date $ qDate qso,
+     ADIF.TimeOn $ qTime qso,
+     ADIF.Freq $ qFreq qso,
+     ADIF.Mode $ qMode qso,
+     ADIF.RST_Received $ qRST_Rcvd qso,
+     ADIF.RST_Sent $ qRST_Sent qso,
+     ADIF.Call $ qCall qso] ++
+
+    -- LOTW wants the band, not the frequency, so just make sure that's
+    -- included here.
+    [ADIF.Band $ freqToBand $ qFreq qso] ++
+
+    -- And now we add in everything that could potentially be set.
+    (catMaybes [qRxFreq qso >>= Just . ADIF.FreqRx,
+                qDXCC qso >>= Just . ADIF.Country,
+                qGrid qso >>= Just . ADIF.Grid,
+                qState qso >>= Just . ADIF.State,
+                qName qso >>= Just . ADIF.Name,
+                qIOTA qso >>= Just . ADIF.Their_IOTA,
+                qITU qso >>= Just . ADIF.ITUZ,
+                qWAZ qso >>= Just . ADIF.CQZ,
+                qSatName qso >>= Just . ADIF.SatelliteName,
+                qSatMode qso >>= Just . ADIF.SatelliteMode])
