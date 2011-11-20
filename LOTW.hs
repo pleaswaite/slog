79f31c8f42e3b9c8883d0f99e6a1d4f3da3e7766
Chris Lumens <chris@bangmoney.org>
2011-11-20 16:05:48 -0500

    Add an upload function to the LOTW module.
    It's lame that I'm using curl for this, but Network.HTTP doesn't support
    https and I don't feel like installing the nine tons of crud that would
    be required, plus then learning to use yet another library.

--- LOTW.hs
+++ LOTW.hs
@@ -1,8 +1,12 @@
-module LOTW(sign) where
+module LOTW(sign,
+            upload)
+ where
 
 import System.Cmd(system)
 import System.Directory(doesFileExist)
 import System.Exit(ExitCode(..))
+import System.FilePath(replaceExtension)
+import System.Process(readProcessWithExitCode)
 
 -- Given a file path and a QTH, sign the file.  Note that you must have set
 -- everything up with tqsl first.
@@ -10,7 +14,7 @@ import System.Exit(ExitCode(..))
 -- NOTE:  tqsl is annoying and will start up graphically, especially if there
 -- are any errors.  It's recommended that the QTH be verified by running this
 -- command.  We can at least verify the file path here, though.
-sign :: String -> FilePath -> IO ()
+sign :: String -> FilePath -> IO FilePath
 sign qth file = do
     exists <- doesFileExist file
 
@@ -19,7 +23,15 @@ sign qth file = do
         -- apparently makes it exit with 255 instead of 0.  Oh well.
         rc <- system $ "tqsl -d -l " ++ qth ++ " " ++ file
         case rc of
-            ExitSuccess     -> return ()
+            ExitSuccess     -> return $ replaceExtension file ".tq8"
             ExitFailure _   -> fail "Signing failed."
     else
         fail "File does not exist."
+
+-- Upload a signed ADIF file to LOTW.
+upload :: FilePath -> IO ()
+upload file = do
+    (exitcode, stdout, stderr) <- readProcessWithExitCode "curl" ["-F", "upfile=@" ++ file, "https://p1k.arrl.org/lotw/upload"] ""
+    case exitcode of
+        ExitSuccess     -> return ()
+        ExitFailure _   -> fail $ "Uploading to LOTW failed: " ++ stderr
