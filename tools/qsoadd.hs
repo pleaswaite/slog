ae006cf8bb2954b7efc2964ab5c1fc46d0267f9d
Chris Lumens <chris@bangmoney.org>
2012-04-22 17:27:56 -0400

    Add a graphical interface to qsoadd, but it doesn't do anything yet.

--- tools/qsoadd.hs
+++ tools/qsoadd.hs
@@ -1,12 +1,18 @@
 {-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
+import Control.Monad(liftM)
 import Data.Char(toUpper)
 import Data.ConfigFile
 import Data.Maybe(fromMaybe)
 import Data.String.Utils(split)
-import System(ExitCode(..), exitWith)
+import Data.Time.Clock(UTCTime(..), getCurrentTime)
+import Data.Time.Format(formatTime)
+import Graphics.UI.Gtk hiding (get, set)
+import Graphics.UI.Gtk.Builder
+import System.Exit(ExitCode(..), exitWith)
 import System.Console.GetOpt
 import System.Directory(getHomeDirectory)
 import System.Environment(getArgs)
+import System.Locale(defaultTimeLocale)
 
 import DB(connect, addQSO)
 import DXCC(DXCC(..), idFromName)
@@ -45,6 +51,7 @@ readConfigFile f = do
 --
 
 data Options = Options {
+    optGraphical :: Bool,
     optDate :: Maybe String,
     optTime :: Maybe String,
     optFreq :: Maybe Double,
@@ -66,10 +73,12 @@ data Options = Options {
     optSatName :: Maybe String,
     optSatMode :: Maybe String }
  deriving (Show)
+
 type OptAction = (Options -> IO Options)
 
 defaultOptions :: Options
 defaultOptions = Options {
+    optGraphical = False,
     optDate = Nothing,
     optTime = Nothing,
     optFreq = Nothing,
@@ -113,8 +122,8 @@ opts = [
 
     Option ['c'] ["dxcc"]       (ReqArg (\arg opt -> return opt { optDXCC = strToInteger arg }) "DXCC")
            "their DXCC entity",
-    Option ['g'] ["grid"]       (ReqArg (\arg opt -> return opt { optGrid = Just arg }) "GRID")
-           "their grid square",
+    Option ['g'] ["graphical"]  (NoArg  (\opt -> return opt { optGraphical = True }))
+           "run in graphical mode",
     Option ['h'] ["help"]       (NoArg  (\_ -> do
                                                    putStrLn (usageInfo "qsoadd" opts)
                                                    exitWith ExitSuccess))
@@ -127,6 +136,8 @@ opts = [
            "their name",
     Option ['o'] ["notes"]      (ReqArg (\arg opt -> return opt { optNotes = Just arg }) "NOTES")
            "notes",
+    Option ['r'] ["grid"]       (ReqArg (\arg opt -> return opt { optGrid = Just arg }) "GRID")
+           "their grid square",
     Option ['s'] ["state"]      (ReqArg (\arg opt -> return opt { optState = Just arg }) "STATE")
            "their state",
     Option ['w'] ["waz"]        (ReqArg (\arg opt -> return opt { optWAZ = strToInteger arg }) "WAZ")
@@ -162,6 +173,146 @@ splitArg s =
     else (Just $ eles !! 0, Just $ eles !! 1)
  where
     eles = split ":" s
+
+--
+-- UI CODE
+--
+
+data ProgramWidgets = ProgramWidgets {
+    pwCall :: Entry,
+    pwRSTRcvd :: Entry,
+    pwRSTSent :: Entry,
+    pwExchangeRcvd :: Entry,
+    pwExchangeSent :: Entry,
+    pwFrequency :: Entry,
+    pwDate :: Entry,
+    pwTime :: Entry,
+
+    pwModeCombo :: ComboBox,
+
+    pwStatus :: Statusbar,
+
+    pwCancel :: Button,
+    pwAdd :: Button }
+
+setDateTime w = do
+    -- Store the current UTC date and time in the appropriate entries.
+    currentUTC <- getCurrentTime
+
+    entrySetText (pwDate w) (formatTime defaultTimeLocale "%F" currentUTC)
+    entrySetText (pwTime w) (formatTime defaultTimeLocale "%R" currentUTC)
+
+setDefaultMode combo = comboBoxSetActive combo 3
+
+-- This function is called when the Cancel button is clicked in order to blank
+-- out the UI and prepare it for starting over.  Expected user behavior is that
+-- Cancel is for when you've entered bad information and need to try again
+-- (missed a QSO, etc.) and quitting the application is for when you want to quit.
+clearUI :: ProgramWidgets -> IO ()
+clearUI w = do
+    -- Blank out all the text entry widgets.
+    let widgets = [pwCall w, pwRSTRcvd w, pwRSTSent w, pwExchangeRcvd w, pwExchangeSent w,
+                   pwFrequency w, pwDate w, pwTime w]
+    mapM_ (flip entrySetText "") widgets
+
+    -- Set the mode combo back to SSB.
+    setDefaultMode (pwModeCombo w)
+
+    -- Remove the status bar message.
+    statusbarRemoveAll (pwStatus w) 0
+
+    setDateTime w
+
+-- Some UI setup is best done in code instead of in the glade file.  This
+-- function is called only once, upon program startup.
+initUI :: ProgramWidgets -> IO ()
+initUI w = do
+    -- Populate the mode combo box with our valid modes.  We don't want to
+    -- support all billion that ADIF does, since that'd be very unwieldy.
+    let combo = pwModeCombo w
+    store <- comboBoxSetModelText combo
+    mapM_ (comboBoxAppendText combo) ["AM", "CW", "FM", "SSB"]
+    setDefaultMode combo
+
+    setDateTime w
+
+buildArgList :: ProgramWidgets -> IO [String]
+buildArgList w = do
+    liftM concat $ sequence [getOneEntry pwCall "-l",
+                             getTwoEntries pwRSTRcvd pwRSTSent "-r",
+                             getTwoEntries pwExchangeRcvd pwExchangeSent "-x",
+                             getOneEntry pwFrequency "-f",
+                             getOneEntry pwDate "-d",
+                             getOneEntry pwTime "-t"]
+ where
+    getEntryText f = do
+        entryGetText (f w)
+    getOneEntry f optName = do
+        s <- getEntryText f
+        if s == "" then return [] else return [optName, s]
+    getTwoEntries f1 f2 optName = do
+        [s1, s2] <- mapM getEntryText [f1, f2]
+        if s1 == "" then return [] else
+            if s2 == "" then return [optName, s1] else return [optName, s1 ++ ":" ++ s2]
+
+-- This function is called when the Add button is clicked in order to add a
+-- QSO into the database.
+addQSOFromUI :: ProgramWidgets -> IO ()
+addQSOFromUI w = do
+    -- Convert the UI elements into a list of command line arguments, like the program
+    -- would normally take.  This is a little lame, but it avoids having to come up with
+    -- all that validation code again.
+    args <- buildArgList w
+
+    -- TODO:  INPUT VALIDATION HERE
+
+    -- Clear out the UI for next time.
+    clearUI w
+
+    -- And update the status bar so the user knows what got added.
+    statusbarPush (pwStatus w) 0 ("QSO added to database.")
+
+    putStrLn $ show args
+
+loadWidgets :: Builder -> IO ProgramWidgets
+loadWidgets builder = do
+    [pwCall, pwRSTRcvd, pwRSTSent, pwExchangeRcvd, pwExchangeSent, pwFrequency,
+     pwDate, pwTime] <- mapM (getO castToEntry)
+                             ["callEntry", "rstRcvdEntry", "rstSentEntry", "exchangeRcvdEntry",
+                              "exchangeSentEntry", "frequencyEntry", "dateEntry", "timeEntry"]
+
+    [pwModeCombo] <- mapM (getO castToComboBox) ["modeComboBox"]
+    [pwCancel, pwAdd] <- mapM (getO castToButton) ["cancelButton", "addButton"]
+    [pwStatus] <- mapM (getO castToStatusbar) ["statusBar"]
+
+    return $ ProgramWidgets pwCall pwRSTRcvd pwRSTSent pwExchangeRcvd pwExchangeSent
+                            pwFrequency pwDate pwTime pwModeCombo pwStatus pwCancel pwAdd
+ where
+    getO cast = builderGetObject builder cast
+
+runGUI :: IO ()
+runGUI = do
+    initGUI
+
+    -- Load the glade file.
+    builder <- builderNew
+    builderAddFromFile builder "data/qsoadd.ui"
+
+    widgets <- loadWidgets builder
+
+    -- Set up the initial state of the UI.
+    initUI widgets
+
+    -- Set up GTK signal handlers to do something.
+    window <- builderGetObject builder castToWindow "window1"
+    onDestroy window mainQuit
+
+    onClicked (pwCancel widgets) $ clearUI widgets
+    onClicked (pwAdd widgets) $ addQSOFromUI widgets
+
+    -- And away we go!
+    widgetShowAll window
+    mainGUI
 
 --
 -- FINALLY, THE MAIN PROGRAM
@@ -224,8 +375,10 @@ main = do
     -- automatically.
     dbh <- connect $ confDB conf
 
-    case optCall cmdline of
-        Just call   -> do ra <- doLookup call (confQTHUser conf) (confQTHPass conf)
-                          addQSO dbh $ buildQSO (fromMaybe emptyRadioAmateur ra) cmdline
-                          return ()
-        _           -> ioError (userError "You must specify a call sign.")
+    -- Are we running in graphical mode or cmdline mode?
+    if optGraphical cmdline then runGUI
+    else case optCall cmdline of
+                  Just call   -> do ra <- doLookup call (confQTHUser conf) (confQTHPass conf)
+                                    addQSO dbh $ buildQSO (fromMaybe emptyRadioAmateur ra) cmdline
+                                    return ()
+                  _           -> ioError (userError "You must specify a call sign.")
