73b15a96102a20f843525bc3c3a2ff2b965b0212
Chris Lumens <chris@bangmoney.org>
2015-01-12 16:27:07 -0500

    A couple fixes to how we sign and upload log files.
    (1) tqsl now actually will run without starting the whole UI if you
    provide -x, as the last option.
    
    (2) tqsl will also do the uploading, so let it.
    
    (3) Use the same haskell function for calling tqsl as we do for
    everything else.

--- tools/qsoupload.hs
+++ tools/qsoupload.hs
@@ -9,13 +9,12 @@ import System.IO
 
 import Slog.DB(getUnsentQSOs, markQSOsAsSent)
 import Slog.Formats.ADIF.Writer(renderRecord)
-import Slog.LOTW(sign, upload)
+import Slog.LOTW(sign)
 import Slog.QSO(qsoToADIF)
 
 import ToolLib.Config
 
 type SignFuncTy = FilePath -> IO FilePath
-type UploadFuncTy = FilePath -> IO ()
 
 withTempFile :: String -> (FilePath -> Handle -> IO a) -> IO a
 withTempFile pattern func = do
@@ -24,13 +23,12 @@ withTempFile pattern func = do
     finally (func tempfile temph)
             (removeFile tempfile)
 
-writeAndUpload :: String -> SignFuncTy -> UploadFuncTy -> FilePath -> Handle -> IO ()
-writeAndUpload adifs signFunc uploadFunc tempname temph = do
+writeAndUpload :: String -> SignFuncTy -> FilePath -> Handle -> IO ()
+writeAndUpload adifs signFunc tempname temph = do
     hPutStrLn temph adifs
     hClose temph
     signedFile <- signFunc tempname
-    finally (uploadFunc signedFile)
-            (removeFile signedFile)
+    removeFile signedFile
 
 main :: IO ()
 main = do
@@ -46,7 +44,7 @@ main = do
     let adifs = intercalate "\r\n" $ map (renderRecord . qsoToADIF) qsos
 
     -- Then write out the temporary file, sign it, and upload it.
-    withTempFile "new.adi" (writeAndUpload adifs (sign $ confQTH conf) upload)
+    withTempFile "new.adi" (writeAndUpload adifs (sign $ confQTH conf))
 
     -- Finally, update the database to reflect everything that's been uploaded.
     markQSOsAsSent fp ids
